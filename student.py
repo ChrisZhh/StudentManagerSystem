19c1e9478892925935f580b9ff1d17cc4ad7ad79
zhh <3088994117@qq.com>
2022-07-28 17:24:47 +0800

    second commit

--- student.py
+++ student.py
@@ -4,10 +4,10 @@ class Student(object):
     """
 
     # 学生类：学号，姓名，电话
-    def __init__(self, num, name, tel):
+    def __init__(self, num, name, age):
         self.num = num
         self.name = name
-        self.tel = tel
+        self.age = age
 
     def __str__(self):
-        return [self.num, f'{self.name}', self.tel]
+        return f"{self.num}, {self.name}, {self.age}"
--- studentmanager.py
+++ studentmanager.py
@@ -45,13 +45,15 @@ class StudentManager(object):
         从文件中加载学员 利用pandas
         """
         df = pd.read_excel('G:/Python/学员信息.xlsx', names=None)
-        data_from_excel = df.values.tolist()
-        for data in data_from_excel:
-            self.student_list.append(data)
-
-        # students = Student(153020198, '李渊大', 25)
-        # self.student_list.append(students.__str__())
-        # print(self.student_list)
+        data_from_excel = df.to_dict(orient='records')
+        for i in data_from_excel:
+            num = i['num']
+            name = i['name']
+            age = i['age']
+            student = Student(num, name, age)
+            self.student_list.append(student)
+        print(data_from_excel)
+        print(self.student_list)
 
     @staticmethod
     def show_main():
@@ -62,15 +64,16 @@ class StudentManager(object):
         print("4：修改学员")
         print("5：显示所有")
         print("6：保存学员")
-        print("6：退出系统")
+        print("7：退出系统")
 
     def student_add(self):
         num = input('请输入学员学号：')
         name = input('请输入学生姓名：')
-        tel = input('请输入学生电话：')
+        age = input('请输入学生年龄：')
 
-        students = Student(num, name, tel)
+        students = Student(num, name, age)
         self.student_list.append(students)
+        self.student_show_all()
 
     def student_search(self):
         name = input('请输入需要查看学生姓名：')
@@ -78,6 +81,8 @@ class StudentManager(object):
         for student in self.student_list:
             if student.name == name:
                 print(student)
+                # 查到了之后跳出循环
+                break
 
     def student_delete(self):
         name = input('请输入需要删除学生姓名：')
@@ -85,24 +90,38 @@ class StudentManager(object):
         for student in self.student_list:
             if student.name == name:
                 self.student_list.remove(student)
+        else:
+            print('该学员不存在')
+        self.student_show_all()
 
     def student_update(self):
         name = input('请输入需要修改学生姓名：')
 
         for student in self.student_list:
             if student.name == name:
-                new_name = input('请输入修改后的名字：')
-                new_num = input('请输入修改后的学号：')
-                new_tel = input('请输入修改后的电话：')
+                student.name = input('请输入修改后的名字：')
+                student.num = input('请输入修改后的学号：')
+                student.tel = input('请输入修改后的年龄：')
+                print(f'修改成功，修改后学员姓名为{student.name},学号是{student.num},年龄是{student.age}')
+                # 修改了之后跳出循环
+                break
+        else:
+            print('该学员不存在')
 
-                student.name = new_name
-                student.num = new_num
-                student.tel = new_tel
+        self.student_show_all()
 
     def student_show_all(self):
+        """
+        显示所有学生
+        """
+        print('姓名\t学号\t年龄')
         for student in self.student_list:
-            print(student)
+            print(f'{student.name}\t{student.num}\t{student.age}')
 
     def student_save(self):
+        data = []
+        for student_data in self.student_list:
+            data.append(student_data)
+        print(data)
         df = pd.DataFrame(self.student_list)
         print(df)
